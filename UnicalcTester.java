cc62c931c3ca4123dca30a29a3c473ea37082d3d
Kirk Srijongsirikul <srijongsirikul.kirk@gmail.com>
2014-05-27 15:22:02 -0700

    Added Unicalc.java and AST.java

--- UnicalcTester.java
+++ UnicalcTester.java
@@ -1,1 +1,16 @@
-Testing
+/*
+ * CSE 12 Homework 8
+ * Joshua Chang and Kirk Srijongsirikul
+ * A11666538 and A11687702
+ * A00 (both partners)
+ * 5-27-14
+ */
+
+public class UnicalcTester extends junit.framework.TestCase
+{
+  protected void setUp()
+  {
+
+  }
+}
+
